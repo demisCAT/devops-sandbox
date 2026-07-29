37f8603958af2791782fbfa58b641c86ef01cb04
demisCAT <josep.barroso@gmail.com>
2026-07-28 22:06:21 -0300

    update greeting message for test purposes v3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from DEMIS !!! DevOps Sandbox!');
+  res.send('Hello from !!!DEMIS !!! DevOps Sandbox!');
 });
 
 app.get('/health', (req, res) => {
